74ff64e0b36c70e89ec948021577dadeef6b39ed
mahi98jat <mahi98jat@gmail.com>
2024-03-07 03:56:31 +0530

    add product details page route

---
+++ ascrip-test/src/app/product-details/page.tsx
@@ -0,0 +1,21 @@
+import { Footer, Header } from "@/components";
+
+import React from "react";
+
+const productDetails = () => {
+  return (
+    <div>
+      <header>
+        <Header />
+      </header>
+      <main>
+        <p>Product details</p>
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
+};
+
+export default productDetails;
--- ascrip-test/src/components/Links/Links.tsx
+++ ascrip-test/src/components/Links/Links.tsx
@@ -2,18 +2,27 @@ import React, { FC } from "react";
 
 import Link from "next/link";
 import styles from "./Links.module.css";
+import { usePathname } from "next/navigation";
 
 const links = [
   { name: "SHOP", path: "/" },
-  { name: "SKILLS", path: "/skills" },
-  { name: "STORIES", path: "/stories" },
-  { name: "ABOUT", path: "/about" },
-  { name: "CONTACT US", path: "/contact" },
+  { name: "SKILLS", path: "/product-details" },
+  { name: "STORIES", path: "/product-details" },
+  { name: "ABOUT", path: "/product-details" },
+  { name: "CONTACT US", path: "/product-details" },
 ];
 const Links: FC<{ layout: "horizontal" | "vertical" }> = ({ layout }) => {
+  const activePathName = usePathname();
+
+  const isActive = (path: string) => activePathName === path;
+
   const LinkMapper = links.map((val, index) => (
     <li>
-      <Link key={index} href={val?.path}>
+      <Link
+        className={isActive(val?.path) ? styles.link : styles.activeLink}
+        key={index}
+        href={val?.path}
+      >
         {val?.name}
       </Link>
     </li>
